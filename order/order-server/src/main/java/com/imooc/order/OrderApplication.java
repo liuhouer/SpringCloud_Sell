f489fa0a0f25630f9e44cb82afff03378304150b
Jingfeng Zhou <908686171@qq.com>
2018-08-01 22:22:51 +0800

    更新order

--- order/order-server/src/main/java/com/imooc/order/OrderApplication.java
+++ order/order-server/src/main/java/com/imooc/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.imooc.order;
 
+import com.imooc.product.client.ProductClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+@EnableFeignClients(basePackageClasses = ProductClient.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
