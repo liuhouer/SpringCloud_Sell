d30dba3fd33539020c47364676e626360c54bebb
Jingfeng Zhou <908686171@qq.com>
2018-08-03 08:03:33 +0800

    配置动态刷新是spring-cloud-bus的一个bug，我不管了，继续学下去吧，少年

---
+++ order/order-server/src/main/java/com/imooc/order/config/GirlConfig.java
@@ -0,0 +1,18 @@
+package com.imooc.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hellozjf
+ */
+@Data
+@Component
+@ConfigurationProperties("girl")
+@RefreshScope
+public class GirlConfig {
+    private String name;
+    private Integer age;
+}
