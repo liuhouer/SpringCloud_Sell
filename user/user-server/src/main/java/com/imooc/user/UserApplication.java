96654c177c7df036bbbda07e793e7d195f8af213
Jingfeng Zhou <908686171@qq.com>
2018-08-07 16:16:37 +0800

    commit user 第九章

--- user/user-server/src/main/java/com/imooc/user/UserApplication.java
+++ user/user-server/src/main/java/com/imooc/user/UserApplication.java
@@ -1,8 +1,13 @@
 package com.imooc.user;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableEurekaClient
--- user/user-server/src/main/java/com/imooc/user/service/impl/UserServiceImpl.java
+++ user/user-server/src/main/java/com/imooc/user/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.imooc.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 /**
  * @author hellozjf
  */
