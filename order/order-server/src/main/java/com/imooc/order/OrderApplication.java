3d98371eb1ee8c2604c3e023d6d3a788839a5929
Jingfeng Zhou <908686171@qq.com>
2018-08-08 08:58:54 +0800

    update

--- order/order-server/src/main/java/com/imooc/order/OrderApplication.java
+++ order/order-server/src/main/java/com/imooc/order/OrderApplication.java
@@ -2,12 +2,13 @@ package com.imooc.order;
 
 import com.imooc.product.client.ProductClient;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableEurekaClient
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
 @EnableFeignClients(basePackageClasses = ProductClient.class)
 public class OrderApplication {
 
--- product/product-server/src/main/java/com/imooc/product/controller/ProductController.java
+++ product/product-server/src/main/java/com/imooc/product/controller/ProductController.java
@@ -87,6 +87,15 @@ public class ProductController {
      */
     @PostMapping("/listForOrder")
     public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList) {
+
+        log.debug("listForOrder productIdList={}", productIdList);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         List<ProductInfo> productInfoList = productService.findList(productIdList);
         log.debug("productIdList={}", productIdList);
         log.debug("productInfoList={}", productInfoList);
